05a46bc38573ef3aa41d944e2c0d41c8aae88418
Kamily1289 <kamilyguedesmariakamilyguedes@gmail.com>
2025-10-21 11:04:14 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // Criação de um objeto da classe Porta
         Porta portaDaFrente = new Porta("Branca", 2.10, 0.90);
 
